66858020f22f6120a72a8cb7e522b10c829a1cb9
Dmytro Bagrii <dimich.dmb@gmail.com>
2022-02-13 05:54:33 +0200

    Remove global mutex

--- spi-ch341-usb.c
+++ spi-ch341-usb.c
@@ -350,8 +350,6 @@ static int ch341_usb_transfer(struct ch341_device *ch341_dev, int out_len, int i
 
 // ----- spi layer begin -------------------------------------------------
 
-static struct mutex ch341_lock;
-
 #define ch341_spi_maser_to_dev(m) *((struct ch341_device**)spi_master_get_devdata(m))
 
 static int ch341_spi_read_inputs (struct ch341_device* ch341_dev)
